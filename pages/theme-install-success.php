573caecca6662dbf86216cda02fbc7886ba6f116
Mark Jaquith <mark@jaquith.me>
2016-09-08 18:45:09 -0400

    Late escaping in the install-success template

--- pages/theme-install-success.php
+++ pages/theme-install-success.php
@@ -5,18 +5,18 @@ require BOLDGRID_BASE_DIR . '/pages/templates/restrict-direct-access.php';
 ?>
 <div class="wrap">
 	<?php if (false == $error) { ?>
-    <h2>Installing Theme: <?php echo $theme_label; ?></h2>
+    <h2>Installing Theme: <?php echo esc_html( $theme_label ); ?></h2>
     <p>
         Downloaded install package...
     </p>
     <p>Unpacked the package…</p>
     <p>Installed the theme…</p>
     <p>
-        Successfully installed the theme <strong><?php echo $theme_label; ?></strong>.
+        Successfully installed the theme <strong><?php echo esc_html( $theme_label ); ?></strong>.
     </p>
     <p>
         <a title="Enable theme for this site"
-        href="<?php echo $enable_theme_url; ?>">Enable Theme</a> |
+        href="<?php echo esc_url( $enable_theme_url ); ?>">Enable Theme</a> |
         <a title="Return to Theme Installer"
             href="<?php echo get_admin_url();?>themes.php">View Themes</a>
     </p>
