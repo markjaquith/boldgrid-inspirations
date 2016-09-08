739f6ec0a47f71d710477c9494b28bcf0565b6e7
Mark Jaquith <mark@jaquith.me>
2016-09-08 18:39:06 -0400

    Standardize on HTTPS and yes-www/no-www for wordpress.org and boldgrid.com

--- pages/attribution.php
+++ pages/attribution.php
@@ -32,7 +32,7 @@ if ( false !== $reseller && ! empty( $reseller['reseller_title'] ) ) {
 
 $attribution_wordpress_and_inspirations = '
 	<p style="clear:both;">
-		%s site was built on a powerful, Inspirations based web builder called <a href="http://www.boldgrid.com">BoldGrid</a>. It is running on <a href="http://wordpress.org">WordPress</a>, the most popular content management software online today.' .
+		%s site was built on a powerful, Inspirations based web builder called <a href="https://www.boldgrid.com/">BoldGrid</a>. It is running on <a href="https://wordpress.org/">WordPress</a>, the most popular content management software online today.' .
 	 $reseller_attribution . '
 	</p>
 ';
